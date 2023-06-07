ede2c8d525c7ef0e8109744243fa022ad488c262
vili <me@vili.dev>
2023-06-07 22:16:56 +0300

    change mod name

--- src/main/java/dev/vili/mixin/TelemetryManagerMixin.java
+++ src/main/java/dev/vili/mixin/TelemetryManagerMixin.java
@@ -1,5 +1,6 @@
 package dev.vili.mixin;
 
+import dev.vili.Main;
 import net.minecraft.client.util.telemetry.TelemetryManager;
 import net.minecraft.client.util.telemetry.TelemetrySender;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,5 +14,6 @@ public class TelemetryManagerMixin {
     @Inject(at = @At("HEAD"), method = "getSender()Lnet/minecraft/client/util/telemetry/TelemetrySender;", cancellable = true)
     private void onGetSender(CallbackInfoReturnable<TelemetrySender> cir) {
         cir.setReturnValue(TelemetrySender.NOOP);
+        Main.LOGGER.info("Telemetry blocked LOL, get fucked microsoft.");
     }
 }
